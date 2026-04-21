0b124f4956a3725a8fe31b17ba28cf40cdd5f0e7
Salvatore Manuel Esposito <salvatoremanuel.esposito@eng.it>
2026-04-21 18:00:12 +0200

    chore (db): import script cliamte cluj-napoca

---
+++ dml/bi_batch_import_tasks/Cluj-Napoca/import_actual_precipitation.sql
@@ -0,0 +1,47 @@
+Cluj-Napoca/Climate Information/Observations/Precipitation/15120099999_precipitation.txt
+Cluj-Napoca/Climate Information/Observations/Precipitation/15120599999_precipitation.txt
+Cluj-Napoca/Climate Information/Observations/Precipitation/15143099999_precipitation.txt
+Cluj-Napoca/Climate Information/Observations/Precipitation/ROE00100902_precipitation.txt
+
+15143099999	9900006	mm
+15120099999	9900005	mm
+ROE00100902	9900004	mm
+15143099999	9900003	°C
+15120599999	9900002	°C
+15120099999	9900001	°C
+ROE00100902	9900000	°C
+15120599999	9900007	mm
+
+INSERT INTO public.batch_job_task_queue (id_batch, json_param, status, date_ins, date_mod) 
+VALUES (3, '{"items":[{"key":"bucket","value":"urbreath-public-repo","type":"STRING"},
+{"key":"objectKey","value":"Cluj-Napoca/Climate Information/Observations/Precipitation/15120099999_precipitation.txt","type":"STRING"},
+{"key":"MEASURE_TYPE","value":"ACTUAL","type":"STRING"},
+{"key":"PARAM_ID","value":"19861","type":"INTEGER"},
+{"key":"SENSOR_ID","value":"9900005","type":"INTEGER"},
+{"key":"PERIOD","value":"1day","type":"STRING"}]}', 0, now(), null);
+
+
+INSERT INTO public.batch_job_task_queue (id_batch, json_param, status, date_ins, date_mod) 
+VALUES (3, '{"items":[{"key":"bucket","value":"urbreath-public-repo","type":"STRING"},
+{"key":"objectKey","value":"Cluj-Napoca/Climate Information/Observations/Precipitation/15120599999_precipitation.txt","type":"STRING"},
+{"key":"MEASURE_TYPE","value":"ACTUAL","type":"STRING"},
+{"key":"PARAM_ID","value":"19861","type":"INTEGER"},
+{"key":"SENSOR_ID","value":"9900007","type":"INTEGER"},
+{"key":"PERIOD","value":"1day","type":"STRING"}]}', 0, now(), null);
+
+INSERT INTO public.batch_job_task_queue (id_batch, json_param, status, date_ins, date_mod) 
+VALUES (3, '{"items":[{"key":"bucket","value":"urbreath-public-repo","type":"STRING"},
+{"key":"objectKey","value":"Cluj-Napoca/Climate Information/Observations/Precipitation/15143099999_precipitation.txt","type":"STRING"},
+{"key":"MEASURE_TYPE","value":"ACTUAL","type":"STRING"},
+{"key":"PARAM_ID","value":"19861","type":"INTEGER"},
+{"key":"SENSOR_ID","value":"9900006","type":"INTEGER"},
+{"key":"PERIOD","value":"1day","type":"STRING"}]}', 0, now(), null);
+
+INSERT INTO public.batch_job_task_queue (id_batch, json_param, status, date_ins, date_mod) 
+VALUES (3, '{"items":[{"key":"bucket","value":"urbreath-public-repo","type":"STRING"},
+{"key":"objectKey","value":"Cluj-Napoca/Climate Information/Observations/Precipitation/ROE00100902_precipitation.txt","type":"STRING"},
+{"key":"MEASURE_TYPE","value":"ACTUAL","type":"STRING"},
+{"key":"PARAM_ID","value":"19861","type":"INTEGER"},
+{"key":"SENSOR_ID","value":"9900004","type":"INTEGER"},
+{"key":"PERIOD","value":"1day","type":"STRING"}]}', 0, now(), null);
+
